2f7e33134c71e72236b71c7584093fb5a9f7b3b4
DANsVAN <d9furj6j@gmail.com>
2025-04-17 22:45:30 -0600

    worked on nav bar5

--- app/Skills/page.jsx
+++ app/Skills/page.jsx
@@ -12,7 +12,7 @@ import { motion } from "framer-motion";
 
 export default function page1() {
   return (
-    <div className=" grid h-screen items-center justify-items-center grid-flow-row grid-cols-3 place-content-center justify-center my-4 sm:my-6 md:my-12 lg:my-32 xl:my-80 gap-20 mx-24 sm:mx-32 md:mx-48 lg:mx-64 xl:mx-80 2xl:mx-96 ">
+    <div className=" grid items-center justify-items-center grid-flow-row grid-cols-3 place-content-center justify-center my-4 sm:my-6 md:my-12 lg:my-32 xl:my-80 gap-20 mx-24 sm:mx-32 md:mx-48 lg:mx-64 xl:mx-80 2xl:mx-96 ">
       <GrideTile img={jsImg}></GrideTile>
       <GrideTile img={pythonimg}></GrideTile>
       <GrideTile img={Cppimg}></GrideTile>
